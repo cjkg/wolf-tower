b930808045b03b9be2a9b51b138487a187c28431
cjkg <cjgibilisco@gmail.com>
2021-06-23 00:24:06 -0500

    mid-proc-gen change

--- main.py
+++ main.py
@@ -9,44 +9,28 @@ from procgen import generate_dungeon
 
 
 def main() -> None:
-    screen_width = 80
-    screen_height = 50
+    screen_width=80
+    screen_height=50
 
-    map_width = 80
-    map_height = 45
-    
-    #proc gen
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
+    map_width=40
+    map_height=40
 
+    tileset=tcod.console_set_custom_font('alloy.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_CP437, 16, 16)
 
-    tileset = tcod.tileset.load_tilesheet(
-        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
-    )
 
     event_handler=EventHandler()
 
-    player=Entity(int(screen_width/2), int(screen_height/2), "@", (255, 236, 39))
-    npc=Entity(int(screen_width/2-5), int(screen_height/2), "@", (255,255,0))
-    entities={player,npc}
+    player=Entity(int(5), int(5), "@", (222, 238, 214))
+    entities={player}
 
-    game_map = generate_dungeon(
-        max_rooms=max_rooms,
-        room_min_size=room_min_size,
-        room_max_size=room_max_size,
-        map_width=map_width,
-        map_height=map_height,
-        player=player
-    )
-
+    game_map=generate_dungeon(10,map_width,map_height)
     engine=Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Wolf Tower",
+        title="WOLF TOWER",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
--- tile_types.py
+++ tile_types.py
@@ -17,6 +17,7 @@ tile_dt = np.dtype(
         ("walkable", np.bool),  # True if this tile can be walked over.
         ("transparent", np.bool),  # True if this tile doesn't block FOV.
         ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("light", graphic_dt),
     ]
 )
 
@@ -26,15 +27,194 @@ def new_tile(
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
     """Helper function for defining individual tile types """
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
+#TODO We'll need to get this so that a function just fills it out, from a dict
+
+# SHROUD represents unexplored, unseen tiles
+SHROUD = np.array((ord(" "), (222, 238, 214), (20, 12, 28)), dtype=graphic_dt)
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (194, 195, 199), (20,12,28)),
+    walkable=True, 
+    transparent=True, 
+    dark=(ord("·"), (48, 52, 109), (20, 12, 28)),
+    light=(ord("·"), (133, 149, 161), (20, 12, 28)),
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord("#"), (131, 118, 156	),(29, 43, 83)),
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("#"), (222, 238, 214), (20, 12, 28)),
+    light=(ord("#"), (222, 238, 214), (20, 12, 28)),
+)
+stairs_up = new_tile(
+    walkable=True, 
+    transparent=True, 
+    dark=(ord(">"), (222, 238, 214), (20, 12, 28)),
+    light=(ord(">"), (210, 125, 44), (20, 12, 28)),
+)
+stairs_down = new_tile(
+    walkable=True, 
+    transparent=True,
+    dark=(ord("<"), (222, 238, 214), (20, 12, 28)),
+    light=(ord("<"), (210, 125, 44), (20, 12, 28)),
+)
+wall_ul_corner = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("╔"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("╔"), (210, 125, 44),(20, 12, 28)),
+)
+wall_horizontal = new_tile(
+    walkable=False, 
+    transparent=False,
+    dark=(ord("═"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("═"), (210, 125, 44),(20, 12, 28)),
+)
+wall_ur_corner = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("╗"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("╗"), (210, 125, 44),(20, 12, 28)),
+)
+
+wall_vertical = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("║"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("║"), (210, 125, 44),(20, 12, 28)),
+)
+
+wall_lr_corner = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("╝"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("╝"), (210, 125, 44),(20, 12, 28)),
+)
+
+wall_ll_corner = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("╚"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("╚"), (210, 125, 44),(20, 12, 28)),
+)
+#TODO this doesn't really fit the categories
+door = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("+"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("+"), (222, 238, 214),(20, 12, 28)),
+)
+
+left_t = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("╠"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("╠"), (210, 125, 44),(20, 12, 28))
+)
+
+right_t = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("╣"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("╣"), (210, 125, 44),(20, 12, 28)),
+)
+bottom_t = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("╩"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("╩"), (210, 125, 44),(20, 12, 28)),
+)
+top_t = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("╦"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("╦"), (210, 125, 44),(20, 12, 28)),
+)
+carpet = new_tile(
+    walkable=True, 
+    transparent=True, 
+    dark=(ord("░"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("░"), (208, 70, 72),(68, 36, 52)),
+)
+vase = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("Φ"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("Φ"), (218, 212, 94),(20, 12, 28)),
+)
+pillar = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("■"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("■"), (20, 12, 28),(210, 125, 44)),
+)
+water = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("≈"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("≈"), (109, 194, 202),(89, 125, 206)),
+)
+pyramid = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("▲"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("▲"), (222, 238, 214),(20, 12, 28)),
+)
+altar = new_tile(
+    walkable=True, 
+    transparent=True, 
+    dark=(ord("╥"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("╥"), (208, 70, 72),(20, 12, 28)),
+)
+herb = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("♣"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("♣"), (52, 101, 36),(20, 12, 28)),
+)
+flower = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("☼"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("☼"), (218, 212, 94),(20, 12, 28)),
+)
+low_wall_ll = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("└"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("└"), (210, 125, 44),(20, 12, 28)),
+)
+low_wall_lr = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("┘"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("┘"), (210, 125, 44),(20, 12, 28)),
+)
+low_wall_vert = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("│"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("│"), (210, 125, 44),(20, 12, 28)),
+)
+low_wall_horiz = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("─"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("─"), (210, 125, 44),(20, 12, 28)),
+)
+low_wall_ul = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("┌"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("┌"), (210, 125, 44),(20, 12, 28)),
+)
+low_wall_ur = new_tile(
+    walkable=False, 
+    transparent=True, 
+    dark=(ord("┐"), (222, 238, 214),(20, 12, 28)),
+    light=(ord("┐"), (210, 125, 44),(20, 12, 28)),
 )
 
